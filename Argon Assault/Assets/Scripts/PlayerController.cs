0e16ae619c27daec3fae7908f88a491ad919bcc1
Marcel Hara <MH261677@FALMOUTH.AC.UK>
2022-04-19 18:21:21 +0100

    Updated Player
    Updated the player prefab with the movement script

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
     //runtime
     private void Update()
     {
-        ThrustMovement();
+        //ThrustMovement();
     }
 
     //Movement
-    private void ThrustMovement()
-    {
-        float horizontalThrust = Input.GetAxis("Horizontal");
-        float verticalThrust = Input.GetAxis("Vertical");
-        Debug.Log(horizontalThrust);
-        Debug.Log(verticalThrust);
-    }
+    //private void ThrustMovement()
+    //{
+    //    //float horizontalThrust = Input.GetAxis("Horizontal");
+    //    //float verticalThrust = Input.GetAxis("Vertical");
+    //    //Debug.Log(horizontalThrust);
+    //    //Debug.Log(verticalThrust);
+    //}
 }
