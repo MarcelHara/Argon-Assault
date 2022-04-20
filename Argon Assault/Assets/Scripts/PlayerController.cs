013f0fab600a43bc897e53907246ba68758a7253
Marcel Hara <MH261677@FALMOUTH.AC.UK>
2022-04-20 11:01:42 +0100

    Updated PlayerController
    Finished movement with clamping so you cant infinitely move away

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //refs and vars
-    Vector2 moveDirection = Vector2.zero;
-    [SerializeField]float playerSpeed = 5f;
+    [SerializeField]float playerSpeed = 25f;
 
     //runtime
     private void Update()
@@ -14,12 +13,25 @@ public class PlayerController : MonoBehaviour
         ThrustMovement();
     }
 
+    private void Start()
+    {
+        transform.localPosition = new Vector3(0, -25, 0); // fixes the position of the object when the timeline edits it
+    }
+
     //Movement
     private void ThrustMovement()
     {
         float xThrust = Input.GetAxis("Horizontal");
         float yThrust = Input.GetAxis("Vertical");
-        Debug.Log(xThrust);
-        Debug.Log(yThrust);
+
+        float xOffset = xThrust * Time.deltaTime * playerSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(newXPos, -10f, 5f);
+
+        float yOffset = yThrust * Time.deltaTime * playerSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+        float clampYPos = Mathf.Clamp(newYPos, -17f, -8f);
+
+        transform.localPosition = new Vector3(clampXPos, clampYPos, transform.localPosition.z);
     }
 }
